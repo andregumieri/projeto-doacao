9f33b5f914f373410fcfa0d49e9f301e2da5618a
André Gumieri <andregumieri@gmail.com>
2021-04-17 12:21:59 -0300

    Make accept header default; func to dump response

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,4 +13,31 @@ abstract class TestCase extends BaseTestCase
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
+
+
+    /**
+     * Adds accept header with application/json by default
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array $parameters
+     * @param array $cookies
+     * @param array $files
+     * @param array $server
+     * @param null $content
+     * @return \Illuminate\Http\Response|void
+     */
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $server['HTTP_ACCEPT'] = 'application/json';
+        parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
+
+    /**
+     * Dumps the reponse content
+     */
+    public function dump()
+    {
+        print_r($this->response->content());
+    }
 }
